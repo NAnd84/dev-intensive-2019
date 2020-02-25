3387b5535a0e141cca2a38512778ecf92c07ed89
ANDREY\Андрей <nikolaenko.and@gmail.com>
2020-02-25 14:46:40 +0400

    Created User data class and implemented its factory. Fix function parseFullName

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,32 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+
+        if(fullName.isNullOrEmpty())
+            return null to null
+
+        var parts: List<String>? = fullName?.split(" ")
+
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+
+        if(firstName.isNullOrEmpty())  firstName = null
+        if(lastName.isNullOrEmpty())  lastName = null
+
+        return firstName to lastName    //Pair(firstName, lastName)
+    }
+
+
+
+    /*
+    fun transliteration(payload: String, divider: String = " "): String {
+        TODO()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        TODO()
+    }
+    */
+
+}
