2a645127909fe306cdcbca8f9fceb2345be045e6
ANDREY\Андрей <nikolaenko.and@gmail.com>
2020-02-28 00:03:03 +0400

    add utility method toInitials()

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,7 +3,7 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
 
-        if(fullName.isNullOrEmpty())
+        if (fullName.isNullOrEmpty())
             return null to null
 
         var parts: List<String>? = fullName?.split(" ")
@@ -11,22 +11,28 @@ object Utils {
         var firstName = parts?.getOrNull(0)
         var lastName = parts?.getOrNull(1)
 
-        if(firstName.isNullOrEmpty())  firstName = null
-        if(lastName.isNullOrEmpty())  lastName = null
+        if (firstName.isNullOrEmpty()) firstName = null
+        if (lastName.isNullOrEmpty()) lastName = null
 
         return firstName to lastName    //Pair(firstName, lastName)
     }
-
 
 
     /*
     fun transliteration(payload: String, divider: String = " "): String {
         TODO()
     }
-
+*/
     fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO()
+        var initials: String? = null
+        if (firstName != null && firstName.isNotEmpty() && firstName[0] != ' ')
+            initials = firstName[0].toUpperCase().toString()
+
+        if (lastName.isNullOrEmpty() || lastName[0] == ' ') return initials
+        initials = lastName[0].toUpperCase().toString()
+
+        return initials
     }
-    */
+
 
 }
