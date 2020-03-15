ccc18dbcf4a14ca55474880f0637654150534354
Andrey <nikolaenko.and@gmail.com>
2020-03-15 13:06:11 +0300

    fix errors

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -16,7 +16,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
         return if (question.answer.contains(answer)) {
             question = question.nextQuestion()
-            "Отлично - ты справился!\n${question.question}" to status.color
+            "Отлично - ты справился\n${question.question}" to status.color
         } else {
             if (status == Status.CRITICAL) {
                 status = Status.NORMAL
